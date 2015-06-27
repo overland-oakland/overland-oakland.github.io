fb20439b0306d8b813736ea03924afc5cdedd529
jnmarcus <jnmarcus872@gmail.com>
2015-06-27 00:03:40 -0700

    add only js files needed, compile, and minify

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,6 +20,8 @@ module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
     // Configurable paths
     config: {
       app: '_app',
@@ -30,8 +32,11 @@ module.exports = function (grunt) {
         files: ['<%= config.app %>/styles/less/**/*.less'],
         tasks: ['less']
         //tasks: ['less', 'autoprefixer', 'csslint', 'cssmin']
+      },
+      js: {
+        files: ['<%= config.app %>/scripts/**/*'],
+        tasks: ['concat:bowerComponents', 'concat:dist', 'uglify']
       }
-      //js: {},
       //jekyll: {},
       //livereload: {}
     },
@@ -56,7 +61,7 @@ module.exports = function (grunt) {
           '<%= config.dist %>/css/main.min.css': ['<%= config.app %>/styles/css/main.css']
         }
       }
-    }
+    },
     //autoprefixer: {
     //  options: {
     //    browsers: ['last 2 versions']
@@ -76,11 +81,52 @@ module.exports = function (grunt) {
     //    src: ['<%= config.dist %>/css/*.css']
     //  }
     //},
-    //cssmin: {}
+    //cssmin: {},
+
+    concat: {
+      options: {
+        // define a string to put between each file in the concatenated output
+        separator: ';'
+      },
+      bowerComponents: {
+        // the files to concatenate
+        src: [
+          '<%= config.app %>/bower_components/jquery/dist/jquery.js',
+          '<%= config.app %>/bower_components/bootstrap/js/transition.js',
+          '<%= config.app %>/bower_components/bootstrap/js/scrollspy.js',
+          '<%= config.app %>/bower_components/bootstrap/js/tab.js',
+          '<%= config.app %>/bower_components/bootstrap/js/collapse.js',
+          '<%= config.app %>/bower_components/bootstrap/js/affix.js',
+          '<%= config.app %>/bower_components/bootstrap-tabcollapse/bootstrap-tabcollapse.js'
+          //'<%= config.dev %>/scripts/lib/*.js'
+        ],
+        // the location of the resulting JS file
+        dest: '<%= config.app %>/scripts/lib/vendor.js'
+      },
+      dist: {
+        src: [
+          '<%= config.app %>/scripts/lib/vendor.js',
+          '<%= config.app %>/scripts/main.js'
+          //'<%= config.dev %>/scripts/lib/*.js'
+        ],
+        dest: '<%= config.dist %>/js/scripts.js'
+      }
+    },
+    uglify: {
+      options: {
+        // the banner is inserted at the top of the output
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          '<%= config.dist %>/js/scripts.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    }
   });
 
   //Define Tasks
-  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('default', ['watch', 'less', 'concat:bowerComponents', 'concat:dist', 'uglify']);
 };
 
 
